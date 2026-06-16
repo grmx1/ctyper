e2e2d6bcb8cf29d386682708473ed48f41cef4cb
grmn13 <germanolmeda1999@gmail.com>
2026-06-16 10:25:07 +0200

    Dynamic text

--- main.c
+++ main.c
@@ -112,23 +112,35 @@ char* parse_file(char* filename, size_t* bsize, int* wcount){
 
 void display_text(char* text, size_t bsize, int idx, int cols, int rows){
 
+	int i = 0;
+
+	if(idx > 0){
+
+		i = (idx / (cols)) * cols;
+	}
+
+	int topl = i + 200 + cols;
+
+	int shalf = 0;
+
+	printf("\033[32m");
+	fflush(stdout);
+
 	//can optimize this here to not send the print statement on every iteration
-	for(int i = 0; i < bsize; i++){
+	for(i; i < bsize && i < topl; i++){
 
-		if(i < idx){
-
-			printf("\033[32m");
-			fflush(stdout);
-		}
-		else if(i == idx){
+		if(i == idx){
 
 			printf("\033[30;47m");
 			fflush(stdout);
 		}
+		else if(i > idx && shalf == 0){
+
+			printf("\033[0m");
+			fflush(stdout);
+		}
 
 		putchar(text[i]);
-		printf("\033[0m");
-		fflush(stdout);
 	}
 }
 
